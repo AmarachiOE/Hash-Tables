1faebbd41d9c9cc66a32e4f6731f0a7f163cb5eb
AmarachiOE <ao.ejiawoko@gmail.com>
2019-08-06 16:04:51 -0700

    messy work in progress. 2 of 4 tests failing.

--- resizing_hashtable/r_hashtables.py
+++ resizing_hashtable/r_hashtables.py
@@ -1,8 +1,8 @@
-
-
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
@@ -17,14 +17,19 @@ class LinkedPair:
 # '''
 class HashTable:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [None] * capacity
 
 
 # '''
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
+    hash = 5381
+    for char in string:
+        hash = ((hash << 5) + hash) + ord(char)
+
+    return hash % max
 
 
 # '''
@@ -33,7 +38,35 @@ def hash(string, max):
 # Hint: Used the LL to handle collisions
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    # first resize
+    # hash_table_resize(hash_table)
+
+    # hash the key
+    index = hash(key, hash_table.capacity)
+    print("Index: ", index, "Key: ", key)
+
+    # get current pair at this index and create new linked pair
+    currentPair = hash_table.storage[index]
+    newPair = LinkedPair(key, value)
+
+    # while bucket not empty and key of currentPair does not match passed in Key
+    while currentPair is not None and currentPair.key != key:
+        currentPair = currentPair.next
+
+    if currentPair and currentPair.key == key:
+        currentPair.value = value
+
+    elif currentPair is None:
+        oldPair = hash_table.storage[index]
+        newPair.next = oldPair
+        hash_table.storage[index] = newPair
+
+        # delete later
+        if oldPair is not None:
+            print("Old Pair Value: ", oldPair.value,
+                  "Old Pair Key: ", oldPair.key)
+        else:
+            print("Old Pair None")
 
 
 # '''
@@ -41,8 +74,26 @@ def hash_table_insert(hash_table, key, value):
 
 # If you try to remove a value that isn't there, print a warning.
 # '''
+
 def hash_table_remove(hash_table, key):
-    pass
+    index = hash(key, hash_table.capacity)
+    print("Hashed Key: ", index)
+
+    # get current pair at this index
+    currentPair = hash_table.storage[index]
+    # prev = None
+
+    if currentPair is not None:
+
+        while currentPair is not None and currentPair.key != key:
+            currentPair = currentPair.next
+
+        if currentPair.key == key:
+            hash_table.storage[index] = currentPair.next
+
+    # set new value of key to None
+    else:
+        print(f"WARNING: {key} does not exist in hash table.")
 
 
 # '''
@@ -51,14 +102,67 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    index = hash(key, hash_table.capacity)
+
+    # if empty bucket return none
+    if not hash_table.storage[index]:
+        return None
+
+    while hash_table.storage[index] is not None:
+        if hash_table.storage[index].key == key:
+            return hash_table.storage[index].value
+
+        # if not, set to .next pair
+        hash_table.storage[index] = hash_table.storage[index].next
 
 
 # '''
 # Fill this in
 # '''
+
 def hash_table_resize(hash_table):
-    pass
+
+    # create empty hash table with 2x capacity of original
+    newHT = HashTable(2 * hash_table.capacity)
+
+    # insert each elem in old ht storage to new ht storage
+    for index in range(len(hash_table.storage)):
+        currentPair = hash_table.storage[index]
+
+        if currentPair != None:
+            hash_table_insert(newHT, currentPair.key,
+                              currentPair.value)  # ht, key, value
+            currentPair = currentPair.next
+
+    return newHT
+
+
+def hash_table_resize1(hash_table):
+
+    filledSpots = 0
+    for i in range(0, len(hash_table.storage)):
+        if hash_table.storage[i] != None:
+            filledSpots += 1
+            # keyName = [key for (key, value) in hash_table.storage[i]]
+            # print("Elem = ", keyName)
+    # print("Filled = ", filledSpots)
+
+    # check if load factor reached
+    if filledSpots >= 0.7 * (hash_table.capacity):
+        # create empty hash table with 2x capacity of original
+        newHT = HashTable(2 * hash_table.capacity)
+        # hash_table.newStorage = [None] * (2*hash_table.capacity) # double capacity
+
+        # insert each elem in old ht storage to new ht storage
+        for elem in range(len(hash_table.storage)):
+            # record = hash_table.storage[elem]
+            # ht, key, value
+            hash_table_insert(newHT, elem, hash_table.storage[elem])
+
+            # then set value in old ht to None
+            hash_table.storage[elem] = None
+
+        return newHT
 
 
 def Testing():
